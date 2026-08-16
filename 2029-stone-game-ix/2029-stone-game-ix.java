aed06af5a4a870a9aa90520bd4d04f731821693b
Saniya <khansaniya06067@gmail.com>
2026-08-16 21:00:36 +0530

    Time: 7 ms (29.05%), Space: 114.9 MB (31.76%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean stoneGameIX(int[] stones) {
+
+        int c0 = 0;
+        int c1 = 0;
+        int c2 = 0;
+
+        for (int stone : stones) {
+
+            if (stone % 3 == 0) {
+                c0++;
+            } 
+            else if (stone % 3 == 1) {
+                c1++;
+            } 
+            else {
+                c2++;
+            }
+        }
+
+        // Even number of remainder-0 stones
+        if (c0 % 2 == 0) {
+            return c1 > 0 && c2 > 0;
+        }
+
+        // Odd number of remainder-0 stones
+        return Math.abs(c1 - c2) > 2;
+    }
+}
